13975c39d7f99a1f5809cb06f3ed87f8874bf215
KeshriAyushi22 <keshri.ayushi10@gmail.com>
2026-06-07 01:24:41 +0530

    tuple in python

---
+++ tuple.py
@@ -0,0 +1,21 @@
+a = (1,2,3)
+
+print(type(a))
+
+#empty tuple
+b = ()
+print(b)
+
+#single tuple => a comma make it a tuple with single entry
+c = (1,)
+print(c)
+
+# immutable, cant change
+# a[0] = ("pirya")
+
+no = a.count(2)
+print(no)
+
+print(a*3)
+print(len(b))
+print(a+a)
