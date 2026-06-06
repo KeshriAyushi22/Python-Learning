f8b3f52788c940b1b1683c37c05816a7a8da1eb5
KeshriAyushi22 <keshri.ayushi10@gmail.com>
2026-06-07 00:47:56 +0530

    string

---
+++ string.py
@@ -0,0 +1,12 @@
+a= 'ayushi'
+b = "keshri"
+c= '''varanasi'''
+
+print(a,b,c)
+
+print(a[0:3]) #characters from 0 to 3 excluding 3
+print(b[1])
+print(c[-1])
+
+print(c[:4]) #from 0 to 4 ; any empty ean whole len
+print(c[1:])
