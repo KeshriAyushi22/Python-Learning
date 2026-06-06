787f58e2a9c61fd68573bc732a6b47cd91ebfe1e
KeshriAyushi22 <keshri.ayushi10@gmail.com>
2026-06-07 00:42:05 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,18 @@
+num = int(input("enter the number to divide: "))
+div = int(input("enter the divider: "))
+
+print(num%div)
+
+val = input("enter value: ")
+print(type(val))
+
+a=34
+b=80
+
+print(a>=b)
+
+# average
+i = int(input("enter the number : "))
+j = int(input("enter the number: "))
+
+print((i+j)/2)
