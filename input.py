a17c06a8629485d9b124cb172f87251b52ca9698
KeshriAyushi22 <keshri.ayushi10@gmail.com>
2026-06-07 00:35:32 +0530

    input in python

---
+++ input.py
@@ -0,0 +1,10 @@
+a = int(input("enter number a: "))
+b = int(input("enter number b: "))
+
+print(a+b)
+
+# string concatinate, always typecast
+a = input("enter number a: ")
+b = input("enter number b: ")
+
+print(a+b)
