da72f5dddaaea55e7a39a3657a0d0f65f6d6b91b
KeshriAyushi22 <keshri.ayushi10@gmail.com>
2026-06-07 01:18:31 +0530

    list in python

---
+++ list.py
@@ -0,0 +1,22 @@
+a = ["ayushi", 5, 1, "1cr", "keshri"]
+
+print(a[3])
+
+#list are mutable
+
+a[4] = "hey"
+
+print(a)
+
+a.append("keshri")
+print(a)
+
+num = [1,6,3,7,2,0,4]
+num.sort()
+print(num)
+
+num.reverse()
+print(num)
+
+num.pop(2)
+print(num)
